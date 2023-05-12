0304fdbf3fd76aee5ee71c55cf85f5731b3bfdce
amir-skoric <amirskoric@outlook.dk>
2023-05-12 05:06:47 +0200

    added anonymous route

---
+++ src/components/AnonymousRoute.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import Loading from './loading/Loading'
+
+const AnonymousRoute = () => {
+  const { currentUser, loading } = useAuth();
+  if (loading) {
+    return <Loading />
+  } else if (!currentUser && !loading) {
+    return <Outlet />;
+  } else {
+    return <Navigate to="/" />;
+  }
+};
+
+export default AnonymousRoute;
